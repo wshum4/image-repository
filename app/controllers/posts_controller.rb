6936c9ea9f4daa0286ccaa10da6af06f5ba90a64
Wince Shum <wince.shum@gmail.com>
2020-09-06 21:33:56 -0400

    added cloudinary

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = policy_scope(Post).order(created_at: :desc)
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, photos: [])
+  end
 end
