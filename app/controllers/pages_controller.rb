6de0f1423e8517202cd2e644a619b3f162654b3a
Wince Shum <wince.shum@gmail.com>
2020-09-07 21:13:40 -0400

    hide buttons from not signed in users took out redirect to login on root page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     # Redirects the user to login page if they are not logged in
-    redirect_to new_user_session_path if current_user.nil?
+    # redirect_to new_user_session_path if current_user.nil?
     # show all posts by all users
     @posts = policy_scope(Post).order(created_at: :desc)
   end
